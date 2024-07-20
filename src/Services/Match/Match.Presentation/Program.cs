806e9d9342c6698202ad34c0e535a512d1aaece0
Sasha <beausas.23@gmail.com>
2024-07-20 03:14:29 +0300

    Fixed mongo connection problems

--- src/Services/Match/Match.Presentation/Program.cs
+++ src/Services/Match/Match.Presentation/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddSignalR();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
